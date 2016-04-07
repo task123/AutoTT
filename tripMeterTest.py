6ac3cecc55e6a2e5d2278a08ca5b27640f59f89e
task123 <hakon.tasken@gmail.com>
2016-04-07 09:34:27 +0200

    f

--- tripMeterTest.py
+++ tripMeterTest.py
@@ -4,8 +4,8 @@ import time
 tripMeter = Motor.TripMeter()
 
 while True:
-    print "right speed: " + tripMeter.get_right_speed()
-    print "left speed: " + tripMeter.get_left_speed()
-    print "right distance: " + tripMeter.get_right_distance()
-    print "left distance: " + tripMeter.get_left_distance()
+    print "right speed: " + str(tripMeter.get_right_speed())
+    print "left speed: " + str(tripMeter.get_left_speed())
+    print "right distance: " + str(tripMeter.get_right_distance())
+    print "left distance: " + str(tripMeter.get_left_distance())
     time.sleep(2)
