d3fd0faa7b58820cc9f506cbc47bad3aa444b26a
task123 <hakon.tasken@gmail.com>
2016-04-07 09:57:10 +0200

    f

--- tripMeterTest.py
+++ tripMeterTest.py
@@ -8,4 +8,6 @@ while True:
     print "left speed: " + str(tripMeter.get_left_speed())
     print "right distance: " + str(tripMeter.get_right_distance())
     print "left distance: " + str(tripMeter.get_left_distance())
+    print "right count: " + str(tripMeter.right_count))
+    print "left count: " + str(tripMeter.left_count))
     time.sleep(2)
