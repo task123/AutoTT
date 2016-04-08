6c65b30f668272900cc863cf42033498503a6e09
Vaagen <pcthrane@stud.ntnu.no>
2016-04-08 16:31:43 +0200

    Update Motor.py

--- Motor.py
+++ Motor.py
@@ -44,7 +44,7 @@ class TripMeter:
         self.right_count_time2 = self.right_count_time1
         self.right_count_time1 = time.time()
         self.right_distance = math.pi * self.wheel_diameter * self.right_count / self.number_of_notches
-        self.right_speed = math.pi * self.wheel_diameter / (2 * self.number_of_notches) / (self.right_count_time1 - self.right_count_time3)
+        self.right_speed = 2 * math.pi * self.wheel_diameter / self.number_of_notches / (self.right_count_time1 - self.right_count_time3)
         
     def left_new_notch(self, channel):
         self.left_count += 1
@@ -52,7 +52,7 @@ class TripMeter:
         self.left_count_time2 = self.left_count_time1
         self.left_count_time1 = time.time()
         self.left_distance = math.pi * self.wheel_diameter * self.left_count / self.number_of_notches
-        self.left_speed = math.pi * self.wheel_diameter / (2 * self.number_of_notches) / (self.left_count_time1 - self.left_count_time3)
+        self.left_speed = 2 * math.pi * self.wheel_diameter / self.number_of_notches / (self.left_count_time1 - self.left_count_time3)
 
     def get_right_distance(self):
         return self.right_distance
