48ae05cd5ee2df1864b276eab84a60f9a17ffc29
Vaagen <pcthrane@stud.ntnu.no>
2016-04-08 20:51:21 +0200

    Update pwmFrequencyTest.py

--- pwmFrequencyTest.py
+++ pwmFrequencyTest.py
@@ -13,8 +13,8 @@ connection = SerialManager(device='/dev/ttyACM0')
 
 Arduino=ArduinoApi(connection=connection)
 
-pin_right = 5
-pin_left = 6
+pin_right = 6
+pin_left = 5
 Arduino.pinMode(pin_right, Arduino.OUTPUT)
 Arduino.pinMode(pin_left, Arduino.OUTPUT)
 
