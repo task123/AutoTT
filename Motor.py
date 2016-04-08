1330537cdbf6219fb0a5f96ea913e0191917eb0f
Vaagen <pcthrane@stud.ntnu.no>
2016-04-08 19:08:35 +0200

    Update Motor.py

--- Motor.py
+++ Motor.py
@@ -79,7 +79,7 @@ class TripMeter:
 class Motor:
     # correct max speed, min voltage
     def __init__(self, trip_meter,  pin_right_forward = 5, pin_right_backward = 10, pin_left_forward = 6, pin_left_backward = 11, 
-            pin_motor_LED = 8, max_speed = 2.0, min_voltage = 1.0, correction_interval = 0.01, 
+            pin_motor_LED = 8, max_speed = 0.55, min_voltage = 1.0, correction_interval = 0.01, 
             proportional_term_in_PID = 0.12, derivative_term_in_PID = 0.001):
         self.trip_meter = trip_meter
         self.connection = SerialManager(device='/dev/ttyACM0')
