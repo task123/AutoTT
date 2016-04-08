fc8467216fd237d8c48db3d2284f151a5840f6d0
Vaagen <pcthrane@stud.ntnu.no>
2016-04-08 19:31:29 +0200

    Create tcpTest.py

---
+++ tcpTest.py
@@ -0,0 +1,14 @@
+import TCP
+import time
+
+class R:
+  def reciever_of_messages(self, message):
+    print message
+
+r = R()
+connection = TCP.Connection(10.22.8.239, 12345, r);
+
+print "hei"
+
+while True:
+  time.sleep(10)
