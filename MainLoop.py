f5e42e7043fc449ee0eb99f2c45bcba861351912
Håkon Taskén <hakon.tasken@gmail.com>
2016-04-08 12:22:10 +0200

    Update MainLoop.py

--- MainLoop.py
+++ MainLoop.py
@@ -10,7 +10,7 @@ trip_meter = Motor.TripMeter()
 motors = Motor.Motor(trip_meter)
 steering = Steering.SteeringWithIOSGyro(motors, autoTTCommunication = autoTTCommunication)
 mode = Steering.Mode(autoTTCommunication, steering)
-status = Status.Status(autoTTCommunication, trip_meter)
+status = Status.Status(autoTTCommunication, motors)
 autoTTCommunication.set_receivers(gyro_recv = steering, mode_recv = mode, status_recv = status, stop_cont_recv = steering)
 
 while True:
