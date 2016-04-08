9801c31cf0c34a1a3dffb673884b7d294d0c757d
Vaagen <pcthrane@stud.ntnu.no>
2016-04-08 14:44:29 +0200

    Update Motor.py

--- Motor.py
+++ Motor.py
@@ -30,10 +30,10 @@ class TripMeter:
         self.right_speed = 0.0
         self.left_speed = 0.0
 
-        GPIO.setmode(GPIO.BOARD)
+        GPIO.setmode(GPIO.BCM)
 
-        GPIO.setup(self.right_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-        GPIO.setup(self.left_pin, GPIO.IN,  pull_up_down=GPIO.PUD_UP)
+        GPIO.setup(self.right_pin, GPIO.IN)
+        GPIO.setup(self.left_pin, GPIO.IN)
         
         GPIO.add_event_detect(self.right_pin, GPIO.RISING, self.right_count)
         GPIO.add_event_detect(self.left_pin, GPIO.RISING, self.left_count)
