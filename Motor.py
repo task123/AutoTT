1bc9226616d55432348f73d0dd834278e0d73dea
Vaagen <pcthrane@stud.ntnu.no>
2016-04-08 14:48:48 +0200

    Update Motor.py

--- Motor.py
+++ Motor.py
@@ -35,10 +35,10 @@ class TripMeter:
         GPIO.setup(self.right_pin, GPIO.IN)
         GPIO.setup(self.left_pin, GPIO.IN)
         
-        GPIO.add_event_detect(self.right_pin, GPIO.RISING, self.right_count)
-        GPIO.add_event_detect(self.left_pin, GPIO.RISING, self.left_count)
+        GPIO.add_event_detect(self.right_pin, GPIO.RISING, self.right_new_notch)
+        GPIO.add_event_detect(self.left_pin, GPIO.RISING, self.left_new_notch)
 
-    def right_count(self, channel):
+    def right_new_notch(channel):
         self.right_count += 1
         self.right_count_time3 = self.right_count_time2
         self.right_count_time2 = self.right_count_time1
@@ -46,7 +46,7 @@ class TripMeter:
         self.right_distance = math.pi * self.wheel_diameter * self.right_count / self.number_of_notches
         self.right_speed = math.pi * self.wheel_diameter / (2 * self.number_of_notches) / (self.right_count_time1 - self.right_count_time3)
         
-    def left_count(self, channel):
+    def left_new_notch(channel):
         self.left_count += 1
         self.left_count_time3 = self.left_count_time2
         self.left_count_time2 = self.left_count_time1
