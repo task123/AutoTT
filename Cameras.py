a47ae165fba4306ce125d3d51b7fc733c7a909be
Håkon Taskén <hakon.tasken@gmail.com>
2016-04-11 20:47:36 +0200

    Update Cameras.py

--- Cameras.py
+++ Cameras.py
@@ -1,4 +1,5 @@
 #! /usr/bin/python
+import cv2
 
 class Cameras:
   def __init__(self):
@@ -10,11 +11,14 @@ class Cameras:
     video2.set(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT,720)
     video1.set(cv2.cv.CV_CAP_PROP_FRAME_WIDTH,1280)
     video2.set(cv2.cv.CV_CAP_PROP_FRAME_WIDTH,1280)
+    self.jpeg_quality = 100
     
   def start(self):
     self.is_looping = True
     if (self.stereo_vision_on = None):
       self.stereo_vision_on = False
+    if (self.stream_on = None):
+      self.stream_on = False
     self.camera_thread = threading.Thread(target = self.camera_loop)
     self.camera_thread.setDaemon(True)
     self.camera_thread.start()
@@ -28,7 +32,10 @@ class Cameras:
       _, image1 = video1.read()
       if (self.stereo_vision_on):
         _, image2 = video2.read()
+      if (self.stream_on):
+        ret, jpeg = cv2.imencode('.jpg', image1, [cv2.cv.CV_IMWRITE_JPEG_QUALITY,self.jpeg_quality])
       
       
   def __del__(self):
-    camera.release()
+    video1.release()
+    video2.release()
