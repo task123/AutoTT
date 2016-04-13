b4170d1762295f7cca2b2222836f77b198b241ed
Håkon Taskén <hakon.tasken@gmail.com>
2016-04-13 13:57:52 +0200

    Update motorTest.py

--- motorTest.py
+++ motorTest.py
@@ -6,6 +6,6 @@ motor = Motor.Motor(trip_meter)
 while True:
     right_speed = input("Set right speed (-100 to 100):")
     left_speed = input("Set left speed (-100 to 100):")
-    motor.set_right_speed(right_speed)
-    motor.set_left_speed(left_speed)
+    #motor.set_right_speed(right_speed)
+    #motor.set_left_speed(left_speed)
     
