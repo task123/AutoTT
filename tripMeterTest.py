b6939221148000b4730adc101288b848c0d42ded
Håkon Taskén <hakon.tasken@gmail.com>
2016-04-13 13:02:10 +0200

    Update tripMeterTest.py

--- tripMeterTest.py
+++ tripMeterTest.py
@@ -2,12 +2,16 @@ import Motor
 import time
 
 tripMeter = Motor.TripMeter()
+motors = Motor.Motor()
 
-while True:
-    print "right speed: " + str(tripMeter.get_right_speed())
-    print "left speed: " + str(tripMeter.get_left_speed())
-    print "right distance: " + str(tripMeter.get_right_distance())
-    print "left distance: " + str(tripMeter.get_left_distance())
-    print "right count: " + str(tripMeter.right_count)
-    print "left count: " + str(tripMeter.left_count)
-    time.sleep(2)
+try:
+    while True:
+        print "right speed: " + str(tripMeter.get_right_speed())
+        print "left speed: " + str(tripMeter.get_left_speed())
+        print "right distance: " + str(tripMeter.get_right_distance())
+        print "left distance: " + str(tripMeter.get_left_distance())
+        print "right count: " + str(tripMeter.right_count)
+        print "left count: " + str(tripMeter.left_count)
+        time.sleep(2)
+except:
+    motors.turn_off()
