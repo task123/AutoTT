d5f0c8f7f517864d8e9009a0dbb266ee4449c30c
task123 <hakon.tasken@gmail.com>
2016-04-13 16:46:11 +0200

    f

--- Steering.py
+++ Steering.py
@@ -147,7 +147,7 @@ class ConnectionTest:
 
     def get_good_connection(self):
         if (self.good_connection):
-            self.good_connection = time.time() - self.time_of_last_connection < 5 * self.intervall
+            self.good_connection = (time.time() - self.time_of_last_connection < 5 * self.intervall)
             if (self.good_connection)
                 return True
             else:
