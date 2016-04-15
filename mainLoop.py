7fd0dcf3703e535920613a59fae704b5aca255f9
Håkon Taskén <hakon.tasken@gmail.com>
2016-04-15 10:50:01 +0200

    Update mainLoop.py

--- mainLoop.py
+++ mainLoop.py
@@ -12,8 +12,9 @@ motors = None
 connection_test = None
 try:
     trip_meter = Motor.TripMeter()
-    autoTTCommunication = TCP.AutoTTCommunication(port, ip_address = ip_address)
+    time.sleep(5)
     motors = Motor.Motor(trip_meter)
+    autoTTCommunication = TCP.AutoTTCommunication(port, ip_address = ip_address)
     steering = Steering.SteeringWithIOSGyro(motors, autoTTCommunication = autoTTCommunication)
     mode = Steering.Mode(autoTTCommunication, steering)
     status = Status.Status(autoTTCommunication, motors)
