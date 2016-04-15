294ebfe08fe94374c09a949c5793ce53a638ded2
Håkon Taskén <hakon.tasken@gmail.com>
2016-04-15 13:30:28 +0200

    Update Steering.py

--- Steering.py
+++ Steering.py
@@ -128,8 +128,7 @@ class Mode:
 
     def send_modes_and_info_modes(self):
         print "send Mode"
-        print self.list_of_modes
-        self.autoTTCommunication.modes(self.list_of_modes)
+        #self.autoTTCommunication.modes(self.list_of_modes)
         #self.autoTTCommunication.info_modes(self.list_of_info_modes)
 
 class ConnectionTest:
