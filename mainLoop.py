3210dbda271422e3d32c86f0c9eb600ddfd4f7d4
Håkon Taskén <hakon.tasken@gmail.com>
2016-04-15 11:42:46 +0200

    Update mainLoop.py

--- mainLoop.py
+++ mainLoop.py
@@ -20,7 +20,7 @@ try:
     connection_test = Steering.ConnectionTest(autoTTCommunication, motors)
     autoTTCommunication.set_receivers(gyro_recv = steering, mode_recv = mode, status_recv = status, 
             stop_cont_recv = steering, disconnect_recv = connection_test, shut_down_recv = connection_test)
-    time.sleep(3)
+    time.sleep(0.5)
     autoTTCommunication.start_gyro_with_update_intervall(1.0/60.0)
     #connection_test.set_intervall(0.05)
     #modes.send_modes_and_info_modes()
