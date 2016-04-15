f023d3a05737f3ca0443c14d8f65af1ea59a5aba
Håkon Taskén <hakon.tasken@gmail.com>
2016-04-15 16:57:37 +0200

    Update Status.py

--- Status.py
+++ Status.py
@@ -77,8 +77,11 @@ class Status:
     def getDiskSpace(self):
         p = os.popen("df -h /")
         i = 0
+        print p
         while 1:
             i = i +1
             line = p.readline()
             if i==2:
+                print line
+                print type(line)
                 return(line.split()[1:5])
