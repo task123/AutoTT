5e9609719f099d09425c43670b4323baed1b601f
Håkon Taskén <hakon.tasken@gmail.com>
2016-04-15 12:32:49 +0200

    Update mainLoop.py

--- mainLoop.py
+++ mainLoop.py
@@ -22,6 +22,7 @@ try:
             stop_cont_recv = steering, disconnect_recv = connection_test, shut_down_recv = connection_test)
     #time.sleep(0.5) # wait for the communication thread to start
     print "main"
+    time.sleep(0.5)
     autoTTCommunication.start_gyro_with_update_intervall(1.0/60.0)
     #connection_test.set_intervall(0.05)
     #modes.send_modes_and_info_modes()
