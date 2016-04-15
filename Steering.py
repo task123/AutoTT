f579ae1f60334ad939e93daa2b94b3bd91bafeb1
Håkon Taskén <hakon.tasken@gmail.com>
2016-04-15 13:31:07 +0200

    Update Steering.py

--- Steering.py
+++ Steering.py
@@ -90,7 +90,7 @@ class SteeringWithIOSButtons:
         elif (type == "Continue"):
             self.stop = False
 
-class Mode:
+class Modes:
     list_of_modes = ["Tilt Steering", "Tilt with AOA", "Button Steering", "Button with AOA", "Follow line", "Stop sign", "Traffic light", "Self steering"] # AOA - Automated Object Avoidence
     
     list_of_info_modes = ["Control the car by tilting your iOS device.", "Control the car by tilting your iOS device while AOA (Automated Object Avoidence) stops you from crashing into objects.", "Control the car by pushing the right and lift side of the screen.", "Control the car by pushing the right and left side of the screen while AOA (Automated Object Avoidence) stops you from crashing into objects.", "The car tries to follow a line on the ground and stops when objects blocks its way.", "The car tries to follow a line on the ground and stops for stop signs and objects blocking its way.", "The car tries to follow a line on the ground and stops for red traffic lights and objects blocking its way.", "The car tries to follow a line on the ground and stops for stop signs, red traffic lights and objects blocking its way."]
