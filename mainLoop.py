59ad9f4cb5ee8c1a4a899b12594a56905a162e75
Håkon Taskén <hakon.tasken@gmail.com>
2016-04-16 19:33:02 +0200

    Update mainLoop.py

--- mainLoop.py
+++ mainLoop.py
@@ -22,7 +22,7 @@ try:
     status = Status.Status(autoTTCommunication, motors, cameras)
     connection_test = TCP.ConnectionTest(autoTTCommunication, motors, cameras)
     autoTTCommunication.set_receivers(gyro_recv = steering, mode_recv = modes, status_recv = status, stop_cont_recv = steering, 
-            disconnect_recv = connection_test, shut_down_recv = connection_test, connection_test_recv)
+            disconnect_recv = connection_test, shut_down_recv = connection_test, connection_test_recv = connection_test)
     time.sleep(0.5) # wait for AutoTT iOS app to start the gyro class
     autoTTCommunication.start_gyro_with_update_intervall(1.0/60.0)
     connection_test.set_intervall(0.05)
