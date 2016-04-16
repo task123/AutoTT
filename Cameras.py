4ad318662be346e0345881e9827eb9d3a5a0ccc7
Håkon Taskén <hakon.tasken@gmail.com>
2016-04-16 15:04:42 +0200

    update camera.py

--- Cameras.py
+++ Cameras.py
@@ -1,40 +1,70 @@
 #! /usr/bin/python
 import cv2
+from nanpy import ArduinoApi
 
 class Cameras:
-    def __init__(self):
-        self.video_1 = cv2.VideoCapture(0)
-        self.video_2 = cv2.VideoCapture(0)
-        self.video_1.set(cv2.CAP_PROP_FPS,30)
-        self.video_2.set(cv2.CAP_PROP_FPS,30)
-        self.video_1.set(cv2.CAP_PROP_FRAME_HEIGHT,720)
-        self.video_2.set(cv2.CAP_PROP_FRAME_HEIGHT,720)
-        self.video_1.set(cv2.CAP_PROP_FRAME_WIDTH,1280)
-        self.video_2.set(cv2.CAP_PROP_FRAME_WIDTH,1280)
+    def __init__(self, motors, pin_battery_camera_1 = 13, pin_battery_camera_2 = 9):
+        self.arduino = motors.arduino
+        self.pin_battery_camera_1 = pin_battery_camera_1
+        self.pin_battery_camera_2 = pin_battery_camera_2
+        
+        self.arduino.pinMode(self.pin_battery_camera_1, arduino.OUTPUT)
+        self.arduino.pinMode(self.pin_battery_camera_2, arduino.OUTPUT)
+        self.arduino.digitalWrite(self.pin_battery_camera_1, 0)
+        self.arduino.digitalWrite(self.pin_battery_camera_2, 0)
+        
         self.jpeg_quality = 100
+        self.fps = 30
+        self.frame_height = 720
+        self.frame_width = 1280
         self.is_looping = True
         self.stream_on = False
-        self.stereo_vision_on = False
-    
-    def start(self):
-        self.is_looping = True
-        if (self.stereo_vision_on == None):
-            self.stereo_vision_on = False
-        if (self.stream_on == None):
-            self.stream_on = False
+        self.camera_1_on = False
+        self.camera_2_on = False
+        
         self.camera_thread = threading.Thread(target = self.camera_loop)
         self.camera_thread.setDaemon(True)
         self.camera_thread.start()
-    
-    def stop(self):
-        self.is_looping = False
-        self.stereo_vision_on = False
+        
+    def turn_on_relay_camera_1(self):
+        self.arduino.digitalWrite(self.pin_battery_camera_1, 1)
+
+    def conditinal_turn_off_relay_camera_1(self):
+        if (not self.camera_1_on):
+            self.arduino.digitalWrite(self.pin_battery_camera_1, 0)
+            
+    def start_camera_1(self):
+        self.turn_on_relay_camera_1()
+        self.video_1 = cv2.VideoCapture(0)
+        self.video_1.set(cv2.CAP_PROP_FPS, self.fps)
+        self.video_1.set(cv2.CAP_PROP_FRAME_HEIGHT, self.frame_height)
+        self.video_1.set(cv2.CAP_PROP_FRAME_WIDTH, self.frame_width)
+        self.camera_1_on = True
+        
+    def stop_camera_1(self):
+        self.camera_1_on = False
+        self.conditinal_turn_off_relay_camera_1()
+        self.video_1.release()
+
+    def start_camera_2(self):
+        self.arduino.digitalWrite(self.pin_battery_camera_2, 1)
+        self.video_2 = cv2.VideoCapture(0)
+        self.video_2.set(cv2.CAP_PROP_FPS, self.fps)
+        self.video_2.set(cv2.CAP_PROP_FRAME_HEIGHT, self.frame_height)
+        self.video_2.set(cv2.CAP_PROP_FRAME_WIDTH, self.frame_width)
+        self.camera_2_on = True
+        
+    def stop_camera_2(self):
+        self.camera_2_on = False
+        self.arduino.digitalWrite(self.pin_battery_camera_2, 0)
+        self.video_2.release()
     
     def camera_loop(self):
-        while self.is_looping:
-            (_, image_1) = self.video_1.read()
-            blur_1 = cv2.GaussianBlur(image_1,(5,5),0)
-            if (self.stereo_vision_on):
+        while True:
+            if (self.camera_1_on):
+                (_, image_1) = self.video_1.read()
+                blur_1 = cv2.GaussianBlur(image_1,(5,5),0)
+            if (self.camera_2_on):
                 _, image2 = self.video2.read()
                 blur_1 = cv2.GaussianBlur(image_2,(5,5),0)
             if (self.stream_on):
@@ -42,7 +72,8 @@ class Cameras:
             cv2.imshow("image", image_1)
 
 
-      
-    def __del__(self):
-        self.video_1.release()
-        self.video_2.release()
+    def turn_off(self):
+        self.stop_camera_2()
+        self.stop_camera_1()
+
+        
