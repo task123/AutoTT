71d1a3d071c43d82143954f95cc9369b3700feda
Håkon Taskén <hakon.tasken@gmail.com>
2016-04-18 22:50:59 +0200

    Update Status.py

--- Status.py
+++ Status.py
@@ -113,22 +113,6 @@ class FanController:
         self.arduino.analogWrite(self.fan_pin, 0)
         while True:
             # temp = float(self.status.getCPUtemperature())
-            """
-            if (temp > self.start_temp):
-                fan_value = (temp - self.start_temp) / (85.0 - self.start_temp) * (self.max_value - self.start_value) + self.start_value
-                if (temp > warning_message_temp and not self.warning_message_sendt):
-                    self.autoTTCommunication.message("The CPU temperatur is over %f C." % (self.warning_message_temp))
-                    self.warning_message_sendt = True
-                self.arduino.analogWrite(self.fan_pin, fan_value)
-            elif (temp < self.stop_temp):
-                self.warning_message_sendt = False
-                self.arduino.analogWrite(self.fan_pin, 0)
-            motor_battery_volt = self.status.getMotorBatteryVolt()
-            if (motor_battery_volt < self.warning_message_motor_battery_volt):
-                self.autoTTCommunication.message("The voltage on the battery driving the motors is under %.2f V" % (motor_battery_volt))
-            raspberry_pi_battery_volt = self.status.getRaspberryPiBatteryVolt()
-            if (raspberry_pi_battery_volt < self.warning_message_raspberry_pi_volt):
-                self.autoTTCommunication.message("The voltage on the battery driving the raspberry pi is under %.2f V" % (raspberry_pi_battery_volt))
-            """
+            
             time.sleep(3)
             
