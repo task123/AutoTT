e64bbc2767ddeb6f62780e65669ae96a09e6f33c
Håkon Taskén <hakon.tasken@gmail.com>
2016-04-18 22:14:02 +0200

    Update Cameras.py

--- Cameras.py
+++ Cameras.py
@@ -100,7 +100,7 @@ class Cameras:
                 self.fps = 15
                 self.frame_height = 720
                 self.frame_width = 1280
-                self.jpeg_quality = 20
+                self.jpeg_quality = 95
                 if (self.video_1 != None):
                     # self.video_1.set(cv2.CAP_PROP_FPS, self.fps)
                     self.video_1.set(cv2.CAP_PROP_FRAME_HEIGHT, self.frame_height)
