b65e2ff36bf64e07b61ef4fab1ae8dbd7fd1a988
Håkon Taskén <hakon.tasken@gmail.com>
2016-04-18 13:06:55 +0200

    Update mainLoop.py

--- mainLoop.py
+++ mainLoop.py
@@ -16,19 +16,19 @@ try:
     trip_meter = Motor.TripMeter()
     motors = Motor.Motor(trip_meter)
     autoTTCommunication = TCP.AutoTTCommunication(port, ip_address = ip_address)
-    steering = Steering.SteeringWithIOSGyro(motors, autoTTCommunication = autoTTCommunication)
-    modes = Steering.Modes(autoTTCommunication, steering)
-    cameras = Cameras.Cameras(motors, streaming_port = port + 1)
-    status = Status.Status(autoTTCommunication, motors)
-    fan_controller = Statur.FanController(motors, status, autoTTCommunication)
+    #steering = Steering.SteeringWithIOSGyro(motors, autoTTCommunication = autoTTCommunication)
+    #modes = Steering.Modes(autoTTCommunication, steering)
+    #cameras = Cameras.Cameras(motors, streaming_port = port + 1)
+    #status = Status.Status(autoTTCommunication, motors)
+    #fan_controller = Statur.FanController(motors, status, autoTTCommunication)
     #connection_test = TCP.ConnectionTest(autoTTCommunication, motors, cameras)
-    autoTTCommunication.set_receivers(gyro_recv = steering, mode_recv = modes, status_recv = status, stop_cont_recv = steering, 
-            disconnect_recv = connection_test, shut_down_recv = connection_test, connection_test_recv = connection_test, 
+    #autoTTCommunication.set_receivers(gyro_recv = steering, mode_recv = modes, status_recv = status, stop_cont_recv = steering, 
+    #        disconnect_recv = connection_test, shut_down_recv = connection_test, connection_test_recv = connection_test, 
             video_recv = cameras)
     time.sleep(0.5) # wait for AutoTT iOS app to start the gyro class
-    autoTTCommunication.start_gyro_with_update_intervall(1.0/60.0)
+    #autoTTCommunication.start_gyro_with_update_intervall(1.0/60.0)
     #connection_test.set_intervall(0.05)
-    modes.send_modes_and_info_modes()
+    # modes.send_modes_and_info_modes()
     
     while True: #connection_test.is_connection_good():
         time.sleep(0.3)
