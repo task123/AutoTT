9c35a0ff3174ea570257f6f6f8be655f1ebadde5
Håkon Taskén <hakon.tasken@gmail.com>
2016-04-18 20:53:09 +0200

    Update Cameras.py

--- Cameras.py
+++ Cameras.py
@@ -97,6 +97,7 @@ class Cameras:
             elif (message == "Off"):
                 self.stop_video_stream()
         if (type == "VideoQuality"):
+            print "VideoQuality"
             if (message == "High"):
                 self.fps = 15
                 self.frame_height = 720
@@ -128,7 +129,7 @@ class Cameras:
             elif (message == "Low"):
                 self.fps = 15
                 self.frame_height = 100
-                self.frame_width = 560
+                self.frame_width = 600
                 self.jpeg_quality = 95
                 if (self.video_1 != None):
                     # self.video_1.set(cv2.CAP_PROP_FPS, self.fps)
