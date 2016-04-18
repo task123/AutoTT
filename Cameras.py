4e98d16225d90906f4c5ab55a788618efd470fd7
Håkon Taskén <hakon.tasken@gmail.com>
2016-04-19 00:31:33 +0200

    Update Cameras.py

--- Cameras.py
+++ Cameras.py
@@ -129,7 +129,8 @@ class Cameras:
                 
     def start_video_stream(self):
         self.opening_video_stream = True
-        time.sleep(1)
+        print "10"
+        time.sleep(10)
         if (not self.camera_1_on):
             self.start_camera_1()
         self.have_yield = False
