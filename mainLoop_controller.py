36e3cc97f763d6cc6b1def1d370648820b6ddc71
Håkon Taskén <hakon.tasken@gmail.com>
2016-04-20 14:49:33 +0200

    Update and rename restart_mainLoop.py to mainLoop_controller.py

--- mainLoop_controller.py
+++ mainLoop_controller.py
@@ -1,16 +1,18 @@
 import os
 import time
 
+os.system("python mainLoop.py")
 file = open("pidMainLoop.txt", "w")
 file.close()
 while True:
   file = open("pidMainLoop.txt")
   pid = file.readline()
   file.close()
-  print pid
   if (pid != '\n' and pid != ''):
     os.kill(int(pid), 2)
     file = open("pidMainLoop.txt", "w")
     file.close()
+    time.sleep(0.01) #make sure mainLoop.py is stopped
+    os.system("python mainLoop.py")
   time.sleep(0.3)
   
