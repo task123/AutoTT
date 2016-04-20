1ad34c34c9e79d4f95b83247a0566b610047f917
Håkon Taskén <hakon.tasken@gmail.com>
2016-04-20 15:27:34 +0200

    Update mainLoop_controller.py

--- mainLoop_controller.py
+++ mainLoop_controller.py
@@ -1,4 +1,3 @@
-import os
 import time
 import subprocess
 import sys
@@ -7,22 +6,14 @@ file = open("pidMainLoop.txt", "w")
 file.close()
 subprocess.Popen([sys.executable, "mainLoop.py"])
 while True:
-  print "start"
   file = open("pidMainLoop.txt")
   pid = file.readline()
   file.close()
-  print pid != ''
-  print pid != '\n'
-  print "mellomrom"
   if (pid != '\n' and pid != ''):
-    print "kill"
     os.kill(int(pid), 2)
     file = open("pidMainLoop.txt", "w")
-    print "1"
-    print "2"
     file.close()
-    time.sleep(1) #make sure mainLoop.py is stopped
-    print "restart mainLoop"
+    time.sleep(0.01) #make sure mainLoop.py is stopped
     subprocess.Popen([sys.executable, "mainLoop.py"])
   time.sleep(0.3)
   
