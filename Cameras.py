b728580302f24cf05dc00007c21a54dc4ad4c97a
Håkon Taskén <hakon.tasken@gmail.com>
2016-04-22 23:31:28 +0200

    Update Cameras.py

--- Cameras.py
+++ Cameras.py
@@ -46,7 +46,7 @@ class Cameras:
     def start_camera_1(self):
         self.arduino.digitalWrite(self.pin_battery_camera_1, 1) # activ high
         time.sleep(5)
-        self.video_1 = cv2.VideoCapture(0)
+        self.video_1 = cv2.VideoCapture(1)
         self.video_1.set(cv2.CAP_PROP_FRAME_HEIGHT, self.frame_height)
         self.video_1.set(cv2.CAP_PROP_FRAME_WIDTH, self.frame_width)
         self.camera_1_on = True
