510c27fafb97c88a5e472fb0bde2e5f8472284b3
Håkon Taskén <hakon.tasken@gmail.com>
2016-04-22 16:03:11 +0200

    Update mainLoop.py

--- mainLoop.py
+++ mainLoop.py
@@ -16,26 +16,32 @@ cameras = None
 fan_controller = None
 disconnect = None
 try:
-    print "1"
     trip_meter = Motor.TripMeter()
-    print "2"
     motors = Motor.Motor(trip_meter)
     print "Ready to login"
     autoTTCommunication = TCP.AutoTTCommunication(port, ip_address = ip_address)
     print "3"
     steering = Steering.SteeringWithIOSGyro(motors, autoTTCommunication = autoTTCommunication)
+    print "4"
     modes = Steering.Modes(autoTTCommunication, steering)
+    print "5"
     cameras = Cameras.Cameras(motors, autoTTCommunication, streaming_port = port + 1)
+    print "6"
     status = Status.Status(autoTTCommunication, motors)
+    print "7"
     fan_controller = Status.FanController(motors, status, autoTTCommunication)
+    print "8"
     disconnect = TCP.Disconnect(autoTTCommunication, motors, cameras, fan_controller)
+    print "9"
     autoTTCommunication.set_receivers(gyro_recv = steering, mode_recv = modes, status_recv = status, stop_cont_recv = steering, 
             disconnect_recv = disconnect, shut_down_recv = disconnect, 
             video_recv = cameras)
+    print "10"
     time.sleep(0.5) # wait for AutoTT iOS app to start the gyro class
     autoTTCommunication.start_gyro_with_update_intervall(1.0/60.0)
     modes.send_modes_and_info_modes()
-
+    print "11"
+    
     while True:
         time.sleep(0.3)
         if (not disconnect.good_connection):
