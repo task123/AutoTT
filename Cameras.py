85bc8738351021515f917e5971841f105abcd750
Håkon Taskén <hakon.tasken@gmail.com>
2016-04-24 13:00:09 +0200

    Update Cameras.py

--- Cameras.py
+++ Cameras.py
@@ -119,9 +119,9 @@ class Cameras:
                     self.video_2.set(cv2.CAP_PROP_FRAME_WIDTH, self.frame_width)
                 self.autoTTCommunication.send_message("VideoStreamRefresh", "")
             elif (message == "Low"):
-                self.frame_height = 240
-                self.frame_width = 360
-                self.jpeg_quality = 95
+                self.frame_height = 300
+                self.frame_width = 568
+                self.jpeg_quality = 20
                 if (self.video_1 != None):
                     self.video_1.set(cv2.CAP_PROP_FRAME_HEIGHT, self.frame_height)
                     self.video_1.set(cv2.CAP_PROP_FRAME_WIDTH, self.frame_width)
