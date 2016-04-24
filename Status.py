e073937e05b452171cba21bf363abf55c2f9f6dd
Håkon Taskén <hakon.tasken@gmail.com>
2016-04-24 17:00:42 +0200

    Update Status.py

--- Status.py
+++ Status.py
@@ -32,7 +32,7 @@ class Status:
         self.arduino.pinMode(self.pin_motor_battery, self.arduino.INPUT)
         self.arduino.pinMode(self.pin_raspberry_pi_battery, self.arduino.INPUT)
         #self.arduino.analogReference(self.arduino.INTERNAL)
-        selv.arduinoVcc = Vcc(RegisterFeature())
+        #selv.arduinoVcc = Vcc(RegisterFeature())
 
     def receive_message(self, type, message):
         if (type == "Status"):
