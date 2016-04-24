9f347ca19c4db3e03fd54bb71e7b9aa5adbb93b2
Håkon Taskén <hakon.tasken@gmail.com>
2016-04-24 13:56:35 +0200

    Update Cameras.py

--- Cameras.py
+++ Cameras.py
@@ -100,7 +100,7 @@ class Cameras:
             if (message == "High"):
                 self.frame_height = 460
                 self.frame_width = 790
-                self.jpeg_quality = 35
+                self.jpeg_quality = 10
                 self.reduse_stream_fps_by_a_factor = 2
                 if (self.video_1 != None):
                     self.video_1.set(cv2.CAP_PROP_FRAME_HEIGHT, self.frame_height)
