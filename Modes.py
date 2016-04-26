537b5d95e6394eb349d7cf1450b34071c01f8d9b
Håkon Taskén <hakon.tasken@gmail.com>
2016-04-26 19:34:32 +0200

    Update Modes.py

--- Modes.py
+++ Modes.py
@@ -61,6 +61,8 @@ class Modes:
                 steering
 
     def send_modes_and_info_modes(self):
+        print "sendt"
+        time.sleep(0.01)
         self.autoTTCommunication.modes(self.list_of_modes)
         time.sleep(0.01) # wait to make sure AutoTT iOS app receive these as two seperate messages
         self.autoTTCommunication.info_modes(self.list_of_info_modes)
