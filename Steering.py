13098970b2375b54e03badc7501250066c2b3d6a
Håkon Taskén <hakon.tasken@gmail.com>
2016-04-26 21:36:38 +0200

    Update Steering.py

--- Steering.py
+++ Steering.py
@@ -245,7 +245,7 @@ class FollowLine:
         self.stopped = True
 
     def find_line(self, speed):
-        self.find_line_thread = threading.Thread(target = self.find_line_loop, args=(speed))
+        self.find_line_thread = threading.Thread(target = self.find_line_loop, args=(speed,))
         self.find_line_thread.setDaemon(True)
         self.find_line_thread.start()
      
