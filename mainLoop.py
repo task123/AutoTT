716fb089ccc4967809de6330ddd226ed1c2d0f35
Håkon Taskén <hakon.tasken@gmail.com>
2016-04-26 19:29:18 +0200

    Update mainLoop.py

--- mainLoop.py
+++ mainLoop.py
@@ -33,26 +33,12 @@ try:
     autoTTCommunication.start_gyro_with_update_intervall(1.0/60.0)
     modes.send_modes_and_info_modes()
     
-    line = "LLL"
     while True:
         time.sleep(0.3)
         if (not disconnect.good_connection):
-            print "hei"
             pid = os.getpid()
-            print "1"
             file = open("pidMainLoop.txt", "w")
-            print "2"
             file.write(str(pid))
-            print "3"
-            file.close()
-            print "4"
-            file = open("pidMainLoop.txt")
-            print "5"
-            line = file.readline()
-            print "6"
-            if (line != ''):
-                print "file"
-            print line
             file.close()
 
 
