ad96ded6edcde83bbf0a09afe7054db01cf39912
Håkon Taskén <hakon.tasken@gmail.com>
2016-04-26 19:26:02 +0200

    Update mainLoop.py

--- mainLoop.py
+++ mainLoop.py
@@ -32,6 +32,7 @@ try:
     autoTTCommunication.start_gyro_with_update_intervall(1.0/60.0)
     modes.send_modes_and_info_modes()
     
+    line = "LLL"
     while True:
         time.sleep(0.3)
         if (not disconnect.good_connection):
