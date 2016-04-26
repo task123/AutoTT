b719aba6fc8f46505698e64c737bae98500d9d5e
Håkon Taskén <hakon.tasken@gmail.com>
2016-04-26 20:06:24 +0200

    Update Modes.py

--- Modes.py
+++ Modes.py
@@ -8,7 +8,9 @@ from nanpy import ArduinoApi
 class Modes:
     list_of_modes = ["Tilt Steering", "Tilt, Lights", "Tilt with AOA", "Button Steering", "Button with AOA", "Follow line", "Stop sign", "Traffic light", "Self steering"] # AOA - Automated Object Avoidence
     
-    list_of_info_modes = ["Control the car by tilting your iOS device.", 
+    list_of_info_modes = ["Tilt Steering", "Tilt, Lights", "Tilt with AOA", "Button Steering", "Button with AOA", "Follow line", "Stop sign", "Traffic light", "Self steering"]
+    """
+    ["Control the car by tilting your iOS device.", 
     "As 'Tilt Stering', but with lights on. The indicators are controlled by pushing the right/left side of the screen and the high beam by pushing both sides of the screen.", 
     "Control the car by tilting your iOS device while AOA (Automated Object Avoidence) stops you from crashing into objects.", 
     "Control the car by pushing the right and lift side of the screen.", 
@@ -17,6 +19,7 @@ class Modes:
     "The car tries to follow a line on the ground and stops for stop signs and objects blocking its way.", 
     "The car tries to follow a line on the ground and stops for red traffic lights and objects blocking its way.", 
     "The car tries to follow a line on the ground and stops for stop signs, red traffic lights and objects blocking its way."]
+    """
     
     def __init__(self, autoTTCommunication, steering, lights):
         self.autoTTCommunication = autoTTCommunication
