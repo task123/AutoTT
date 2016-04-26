c6e42bf8a386f2aed307468a6a5dcd97a1170206
Håkon Taskén <hakon.tasken@gmail.com>
2016-04-26 19:28:45 +0200

    Update mainLoop_controller.py

--- mainLoop_controller.py
+++ mainLoop_controller.py
@@ -12,16 +12,12 @@ subprocess.Popen([sys.executable, "mainLoop.py"])
 while True:
   file = open("pidMainLoop.txt")
   pid = file.readline()
-  print len(pid)
-  print pid == ''
   file.close()
   if (pid != '\n' and pid != ''):
-    print "kill"
     os.kill(int(pid), 2)
     file = open("pidMainLoop.txt", "w")
     file.close()
     time.sleep(1) #make sure mainLoop.py is stopped
     subprocess.Popen([sys.executable, "mainLoop.py"])
-    print "sub"
   time.sleep(0.3)
   
