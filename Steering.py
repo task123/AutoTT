ec2c1626b04287fa607f5f29a999fcf07bd46122
Vaagen <pcthrane@stud.ntnu.no>
2016-04-26 23:34:54 +0200

    Update Steering.py

--- Steering.py
+++ Steering.py
@@ -174,7 +174,7 @@ class FollowLine:
         self.left_photo_diode_found_line_value = 70
         self.right_photo_diode_found_line_value = 140
         self.target_value_left_photo_diode = 100
-        self.target_value_right_photo_diode = 100
+        self.target_value_right_photo_diode = 160
         self.correction_interval = 0.01
         # these values might change
         self.pin_photo_diode_power = 12
