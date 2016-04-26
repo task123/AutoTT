752b3fb2fe94790f192e679138501ce8158f1a02
Håkon Taskén <hakon.tasken@gmail.com>
2016-04-26 21:26:49 +0200

    Create followLineTest.py

---
+++ followLineTest.py
@@ -0,0 +1,16 @@
+import TCP
+import Motor
+import Steering
+import Status
+import time
+import Cameras
+import Lights
+import Modes
+import os
+
+trip_meter = Motor.TripMeter()
+motors = Motor.Motor(trip_meter)
+follow_line = Steering.FollowLine(10)
+
+while True:
+  time.sleep(10)
