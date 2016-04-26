9e16e826a6f6fcbe81f74c1b464fbbd2e53a96bd
task123 <hakon.tasken@gmail.com>
2016-04-26 12:13:47 +0200

    f

--- disparityTest.py
+++ disparityTest.py
@@ -59,8 +59,8 @@ num_disp = 112-min_disp
     speckleRange = 32
     )
     """
-#stereo = cv2.StereoBM(cv2.STEREO_BM_BASIC_PRESET,ndisparities=96, SADWindowSize=25)
-stereo = cv2.createStereoBM(numDisparities = 96, blockSize = 25 ) #blockSize must be odd
+stereo = cv2.StereoBM(cv2.STEREO_BM_BASIC_PRESET,ndisparities=96, SADWindowSize=25)
+#stereo = cv2.createStereoBM(numDisparities = 96, blockSize = 25 ) #blockSize must be odd
 
 print('computing disparity...')
 imgL = cv2.cvtColor(imgL, cv2.COLOR_BGR2GRAY)
