5f39d97f8be2a8c86bff6ccc3d8970b7405fe7a5
Håkon Taskén <hakon.tasken@gmail.com>
2016-04-26 17:43:39 +0200

    Update Lights.py

--- Lights.py
+++ Lights.py
@@ -6,25 +6,25 @@ from nanpy import SerialManager
 class Lights:
   def __init__(self, motors):
     # these variables might need to be adjusted
-    low_beam_value = 5
-    high_beam_value = 400
-    indicator_blink_time = 0.1
-    indicator_blink_delay = 0.1
+    self.low_beam_value = 5
+    self.high_beam_value = 400
+    self.indicator_blink_time = 0.1
+    self.indicator_blink_delay = 0.1
     # these variables might change
-    headlight_pin = 9
-    tail_light_pin = 17
-    right_indicator_pin = 2
-    left_indicator_pin = 16
+    self.headlight_pin = 9
+    self.tail_light_pin = 17
+    self.right_indicator_pin = 2
+    self.left_indicator_pin = 16
     ##################################################
     # Values after this should not need to be changed.
     ##################################################
 
     self.arduino = motors.arduino
 
-    self.arduino.pinMode(headlight_pin, self.arduino.OUTPUT)
-    self.arduino.pinMode(tail_light_pin, self.arduino.OUTPUT)
-    self.arduino.pinMode(right_indicator_pin, self.arduino.OUTPUT)
-    self.arduino.pinMode(left_indicator_pin, self.arduino.OUTPUT)
+    self.arduino.pinMode(self.headlight_pin, self.arduino.OUTPUT)
+    self.arduino.pinMode(self.tail_light_pin, self.arduino.OUTPUT)
+    self.arduino.pinMode(self.right_indicator_pin, self.arduino.OUTPUT)
+    self.arduino.pinMode(self.left_indicator_pin, self.arduino.OUTPUT)
     
     self.is_lights_on = False
     self.is_high_beam_on = False
@@ -34,31 +34,31 @@ class Lights:
   def lights_on(self):
     self.is_lights_on = True
     if (self.is_high_beam_on):
-      self.arduino.analogWrite(headlight_pin, high_beam_value)
+      self.arduino.analogWrite(self.headlight_pin, self.high_beam_value)
     else:
-      self.arduino.analogWrite(headlight_pin, low_beam_value)
-    self.arduino.digitalWrite(tail_light_pin, 1)
+      self.arduino.analogWrite(self.headlight_pin, self.low_beam_value)
+    self.arduino.digitalWrite(self.tail_light_pin, 1)
     
   def lights_off(self):
     self.is_lights_on = False
     self.is_high_beam_on = False
     self.is_right_indicator_on = False
     self.is_left_indicator_on = False
-    self.arduino.analogWrite(headlight_pin, 0)
-    self.arduino.digitalWrite(tail_light_pin, 0)
+    self.arduino.analogWrite(self.headlight_pin, 0)
+    self.arduino.digitalWrite(self.tail_light_pin, 0)
 
     
   def high_beam_on(self):
     if (self.is_lights_on):
       self.is_high_beam_on = True
-      self.arduino.analogWrite(headlight_pin, high_beam_value)
+      self.arduino.analogWrite(self.headlight_pin, self.high_beam_value)
     
   def high_beam_off(self):
     self.is_high_beam_on = False
     if (self.is_lights_on):
-      self.arduino.analogWrite(headlight_pin, low_beam_value)
+      self.arduino.analogWrite(self.headlight_pin, self.low_beam_value)
     else:
-      self.arduino.analogWrite(headlight_pin, 0)
+      self.arduino.analogWrite(self.headlight_pin, 0)
       
   def right_indicator_loop(self):
     while (self.is_right_indicator_on):
