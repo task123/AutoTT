a6652459461b40c2c6d754f55a437a980275b536
Håkon Taskén <hakon.tasken@gmail.com>
2016-04-27 06:25:56 +0200

    Update Modes.py

--- Modes.py
+++ Modes.py
@@ -12,9 +12,7 @@ import Motor
 class Modes:
     list_of_modes = ["Tilt Steering", "Tilt, Lights", "Tilt with AOA", "Button Steering", "Button with AOA", "Follow line", "Stop sign", "Traffic light", "Self steering"] # AOA - Automated Object Avoidence
     
-    list_of_info_modes = ["Tilt Steering", "Tilt, Lights", "Tilt with AOA", "Button Steering", "Button with AOA", "Follow line", "Stop sign", "Traffic light", "Self steering"]
-    """
-    ["Control the car by tilting your iOS device.", 
+    list_of_info_modes = ["Control the car by tilting your iOS device.", 
     "As 'Tilt Stering', but with lights on. The indicators are controlled by pushing the right/left side of the screen and the high beam by pushing both sides of the screen.", 
     "Control the car by tilting your iOS device while AOA (Automated Object Avoidence) stops you from crashing into objects.", 
     "Control the car by pushing the right and lift side of the screen.", 
@@ -23,7 +21,6 @@ class Modes:
     "The car tries to follow a line on the ground and stops for stop signs and objects blocking its way.", 
     "The car tries to follow a line on the ground and stops for red traffic lights and objects blocking its way.", 
     "The car tries to follow a line on the ground and stops for stop signs, red traffic lights and objects blocking its way."]
-    """
     
     def __init__(self, autoTTCommunication, motors,  lights, steering, cameras, status, disconnect):
         self.autoTTCommunication = autoTTCommunication
@@ -50,17 +47,12 @@ class Modes:
                 steering.button_indicators_off()
                 autoTTCommunication.buttons_off()
             elif (message == "1"): # Tilt, Lights
-                print "yess"
                 self.steering = Steering.SteeringWithIOSGyro(self.motors)
-                print "1"
                 self.autoTTCommunication.set_receivers(gyro_recv = self.steering, mode_recv = self, status_recv = self.status, 
                     stop_cont_recv = self.steering, disconnect_recv = self.disconnect, shut_down_recv = self.disconnect, 
                     video_recv = self.cameras, button_recv = self.steering)
-                print "2"
                 self.steering.button_indicators_on(self.lights)
-                print "3"
                 self.autoTTCommunication.buttons_on()
-                print "4"
             elif (message == "2"): # Tilt with AOA
                 self.steering = Steering.SteeringWithIOSGyro(self.motors)
                 self.autoTTCommunication.set_receivers(gyro_recv = self.steering, mode_recv = self, status_recv = self.status, 
