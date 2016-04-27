e5bf8fe34836568e069d1e90c80c2cffd07eb98f
Håkon Taskén <hakon.tasken@gmail.com>
2016-04-27 23:52:44 +0200

    Update motorTest.py

--- motorTest.py
+++ motorTest.py
@@ -9,6 +9,7 @@ try:
         left_speed = input("Set left speed (-100 to 100):")
         motor.set_right_speed(right_speed)
         motor.set_left_speed(left_speed)
+        print "%f   %f" % (trip_meter.get_right_speed(), trip_meter.get_left_speed())
 except:
     motor.set_right_speed(0)
     motor.set_left_speed(0)
