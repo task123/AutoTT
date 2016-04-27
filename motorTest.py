6333da1dff8610410adaf40524f9a2e1c7158335
Håkon Taskén <hakon.tasken@gmail.com>
2016-04-27 23:56:39 +0200

    Update motorTest.py

--- motorTest.py
+++ motorTest.py
@@ -1,7 +1,19 @@
 import Motor
+import threading
+import time
+
+def print_speed_loop(trip_meter):
+    while True: 
+        print "%f   %f" % (trip_meter.get_right_speed(), trip_meter.get_left_speed())
+        time.sleep(0.1)
+        
 
 trip_meter = Motor.TripMeter()
 motor = Motor.Motor(trip_meter)
+
+print_speed_thread = threading.Thread(target = print_speed_loop)
+print_speed_thread.setDaemon(True)
+print_speed_thread.start()
 
 try: 
     while True:
@@ -9,7 +21,6 @@ try:
         left_speed = input("Set left speed (-100 to 100):")
         motor.set_right_speed(right_speed)
         motor.set_left_speed(left_speed)
-        print "%f   %f" % (trip_meter.get_right_speed(), trip_meter.get_left_speed())
 except:
     motor.set_right_speed(0)
     motor.set_left_speed(0)
