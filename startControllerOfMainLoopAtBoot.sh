aa80798fd443df03e3ccb5c574f37870d04b83fb
Håkon Taskén <hakon.tasken@gmail.com>
2016-05-01 18:10:30 +0200

    Update startControllerOfMainLoopAtBoot.sh

--- startControllerOfMainLoopAtBoot.sh
+++ startControllerOfMainLoopAtBoot.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-echo -e "GET http://google.com HTTP/1.0\n\n" | nc google.com 80 > /dev/null 2>&1
+# check for internett connection and the start the controllerOfMainLoop.py as the mainLoop.py need 
+# echo -e "GET http://google.com HTTP/1.0\n\n" | nc google.com 80 > /dev/null 2>&1
 
-while [ ! $? -eq 0 ]
-do 
-  echo -e "GET http://google.com HTTP/1.0\n\n" | nc google.com 80 > /dev/null 2>&1
-done
-
+# while [ ! $? -eq 0 ]
+# do 
+  # echo -e "GET http://google.com HTTP/1.0\n\n" | nc google.com 80 > /dev/null 2>&1
+# done
+sleep 10
 python controllerOfMainloop.py
