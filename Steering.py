e4c86c540e52090e27c4f097fb9fba72462d38a0
Håkon Taskén <hakon.tasken@gmail.com>
2016-05-02 16:29:20 +0200

    Update Steering.py

--- Steering.py
+++ Steering.py
@@ -207,8 +207,8 @@ The car must start off the line, such that it will cross it when driving straigh
 class FollowLine:
     def __init__(self, motors, speed = 12):
         # these values might need to be adjusted
-        self.proportional_term_in_PID = 7.0 # 0.12
-        self.derivative_term_in_PID = 0.01 # 0.001
+        self.proportional_term_in_PID = 0.7 # 0.12
+        self.derivative_term_in_PID = 0.001 # 0.001
         self.part_off_new_error_used_in_smoothing = 0.3
         self.left_photo_diode_found_black_line_value = 150
         self.right_photo_diode_found_black_line_value = 130
