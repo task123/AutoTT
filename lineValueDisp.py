95a128e5ae5b5ac0788a82936964873cecce8118
Håkon Taskén <hakon.tasken@gmail.com>
2016-05-02 10:57:46 +0200

    Update lineValueDisp.py

--- lineValueDisp.py
+++ lineValueDisp.py
@@ -1,21 +1,29 @@
-
-import TCP
-import Motor
-import Steering
-import Status
+import threading
+from nanpy import ArduinoApi
+from nanpy import SerialManager
+import math
 import time
-import Cameras
-import Lights
-import Modes
-import os
+import Motor
+
+trip_meter = Motor.TripMeter()
+motors = Motor.Motor(trip_meter)
+
+pin_photo_diode_power = 12
+pin_left_photo_diode = 18
+pin_right_photo_diode = 19
+
+arduino = motors.arduino
+            
+arduino.pinMode(pin_photo_diode_power, arduino.OUTPUT)
+arduino.pinMode(pin_left_photo_diode, arduino.INPUT)
+arduino.pinMode(pin_right_photo_diode, arduino.INPUT)
+
+arduino.digitalWrite(pin_photo_diode_power, 1)
 
 try:
-  trip_meter = Motor.TripMeter()
-  motors = Motor.Motor(trip_meter)
-  follow_line = Steering.FollowLine(motors, start_speed = 0)
-
-  while True:
-    time.sleep(10)
+  while (True):
+    print str(analogRead(pin_right_photo_diode)) + "    " + str(analogRead(pin_right_photo_diode))
+    time.sleep(0.1)
 except:
-  motors.turn_off()
-  follow_line.stop()
+  motors.turn_off
+  arduino.digitalWrite(pin_photo_diode_power, 0)
