d35a792462ce5ade86ca963858d8843423447ad6
Håkon Taskén <hakon.tasken@gmail.com>
2016-05-02 18:54:58 +0200

    Update Steering.py

--- Steering.py
+++ Steering.py
@@ -210,7 +210,7 @@ class FollowLine:
         # these values might need to be adjusted
         self.proportional_term_in_PID = 1.0 
         self.derivative_term_in_PID = 0.001
-        self.part_off_new_error_used_in_smoothing = 0.0 #0.18
+        self.part_off_new_error_used_in_smoothing = 1.0 #0.18
         self.left_photo_diode_found_black_line_value = 145
         self.right_photo_diode_found_black_line_value = 125
         self.left_photo_diode_found_white_line_value = 390
@@ -281,8 +281,8 @@ class FollowLine:
                     
                 self.error = self.part_off_new_error_used_in_smoothing * self.new_error + (1.0 - self.part_off_new_error_used_in_smoothing) * self.error
                 
-                self.left_speed = self.speed - self.error*self.proportional_term_in_PID + (self.error - self.previous_error)*self.derivative_term_in_PID/self.correction_interval
-                self.right_speed = self.speed + self.error*self.proportional_term_in_PID - (self.error - self.previous_error)*self.derivative_term_in_PID/self.correction_interval
+                self.left_speed = self.speed * (1.0 - self.error*self.proportional_term_in_PID + (self.error - self.previous_error)*self.derivative_term_in_PID/self.correction_interval)
+                self.right_speed = self.speed * (1.0 + self.error*self.proportional_term_in_PID - (self.error - self.previous_error)*self.derivative_term_in_PID/self.correction_interval)
                 
                 if (self.left_speed > 100 or self.right_speed > 100):
                     if (self.left_speed > self.right_speed):
