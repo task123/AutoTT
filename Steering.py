15b63566f069c445bc1a178e06a1bc5273aeacb6
Håkon Taskén <hakon.tasken@gmail.com>
2016-05-02 15:27:43 +0200

    Update Steering.py

--- Steering.py
+++ Steering.py
@@ -212,8 +212,8 @@ class FollowLine:
         self.part_off_new_error_used_in_smoothing = 0.3
         self.left_photo_diode_found_black_line_value = 90
         self.right_photo_diode_found_black_line_value = 130
-        self.left_photo_diode_found_white_line_value = 360
-        self.right_photo_diode_found_white_line_value = 440
+        self.left_photo_diode_found_white_line_value = 350
+        self.right_photo_diode_found_white_line_value = 430
         self.right_photo_diode_lowest_line_value = 112.0
         self.left_photo_diode_lowest_line_value = 79.0
         self.right_photo_diode_at_lowest_left_value = 331.0
