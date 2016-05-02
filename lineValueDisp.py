9085ce6c9ccb0451d1323dd8650f79dea4d6b72c
Håkon Taskén <hakon.tasken@gmail.com>
2016-05-02 16:02:05 +0200

    Update lineValueDisp.py

--- lineValueDisp.py
+++ lineValueDisp.py
@@ -23,14 +23,14 @@ try:
     right_value = (arduino.analogRead(pin_right_photo_diode) - 112.0) / 331.0
     left_value = (arduino.analogRead(pin_left_photo_diode) - 79.0) / 272.0
     error = 0
-    #print str(arduino.analogRead(pin_right_photo_diode)) + "    " + str(arduino.analogRead(pin_left_photo_diode)) # + "    " + str(right_value + left_value) + "    " + str(right_value - left_value)
+    print str(arduino.analogRead(pin_right_photo_diode)) + "    " + str(arduino.analogRead(pin_left_photo_diode)) # + "    " + str(right_value + left_value) + "    " + str(right_value - left_value)
     if (right_value > 1):
       error = 2 * right_value
     elif (left_value > 1):
       error = - 2 * left_value
     else:
       error = right_value - left_value
-    print error
+    #print error
     time.sleep(0.1)
 except:
   motors.turn_off
