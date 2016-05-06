a72a1c2cc43e77bdce2f2f74a7817628170f5ff3
Håkon Taskén <hakon.tasken@gmail.com>
2016-05-06 17:48:54 +0200

    Update Steering.py

--- Steering.py
+++ Steering.py
@@ -277,7 +277,6 @@ class FollowLine:
         print "following line"
         while not self.quit:
             if (self.stopped):
-                print "stopped"
                 self.motors.stop()
             else:
                 self.new_error = self.arduino.analogRead(self.pin_left_photo_diode) - self.arduino.analogRead(self.pin_right_photo_diode)
@@ -300,13 +299,14 @@ class FollowLine:
                     print "max"
                     self.right_speed = self.speed + difference / abs(difference) * self.max_turn / 2.0 * self.speed
                     self.left_speed = self.speed - difference / abs(difference) * self.max_turn / 2.0 * self.speed
+                """
                 print str(self.left_speed) + "    " + str(self.right_speed)
                 self.motors.set_left_speed(self.left_speed)
                 self.motors.set_right_speed(self.right_speed)
-                """
+                
                 self.previous_error = self.error
                 #print self.error
-                
+                """
                 if (self.is_at_junction() and (self.is_turning_left or self.is_turning_right) and not self.have_detected_junction):
                     self.trip_meter.reset()
                     self.have_detected_junction = True
@@ -319,7 +319,7 @@ class FollowLine:
                     self.have_detected_junction = False
                     self.is_turning_right = False
                     self.turn_right()
-                    
+                """
             time.sleep(self.correction_interval)
             
     def is_at_junction(self):
